726a6cd44aefb822d32011ede1c52e6545660de9
Yizheng Wu <Yizheng@campus-032-000.ucdavis.edu>
2017-02-10 11:37:56 -0800

    Pulling out the build-in parameters into .csv

--- 00_R scripts/Sac_ITHIM_Injuries.R
+++ 00_R scripts/Sac_ITHIM_Injuries.R
@@ -3,20 +3,20 @@
 ##### Injury Module #####
 
 #set your working directory
-setwd("~/Documents/02_Work/13_ITHIM/03_Data/02_Injury")
+setwd("~/Documents/02_Work/14_GitHub/00_ITHIM/01_Data")
 
 # Prevent scientific notation
 options(scipen = 100)
 
 # read the csv of injury data
 # data source: SWITRS 2006-2010 (Statewide Integrated Traffic Reporting System: 5-year annual average number of road traffic injuries)
-temp <- read.table("Injury.csv",header=T,sep=',')
+temp <- read.table("05_Injury.csv",header=T,sep=',')
 injury <- temp[,2:10]
   
 # number of traffic modes, injuried types, and road types
-nTrafficModeV <- 8 #victim mode
-nTrafficModeS <- 9 #striking mode (include NOV)
-nInjuriedType <- 2 #fatal & serious
+nTrafficModeV <- 8L #victim mode
+nTrafficModeS <- 9L #striking mode (include NOV)
+nInjuriedType <- 2L #fatal & serious
 ModeNames <- colnames(injury)
 RoadTypes <- c("local","arterial","highway")
 nRoadType <- length(RoadTypes)
@@ -40,72 +40,95 @@ createBaselineInjury <- function(){
 ##### Distribution of Person & Vehicle Distance by Road Types  #####
 
 #create the sheet "Dist by road type" in ITHIM spreadsheet 
-createDistribution <- function(scenario.no = 1){
+createDistribution <- function(){
   # person distance for all modes (miles per day)
   # source: CHTS2012 & NHTS2009
-  t.person.distance.baseline <- c(0.2314295,0.16619841,0.43905542,27.0677899,0.08906508,1.70441975,NA,0.12788347)
+  person.vehicle.distance_input <- read.csv("06_PersonVehicleDistance.csv")
+
+  # for baseline
+  t.person.distance.baseline <- person.vehicle.distance_input[1:8,3]
   
   # matrix of distance (person) distribution by road types and modes for baseline (%)
   # data source: for walk and bike, hard coded estimate; for other modes, CHTS2012
   precent.person.baseline <- matrix(NA,nrow=nTrafficModeV,ncol=3,dimnames=list(ModeNames[1:8],c("local","arterial","highway")))
-  precent.person.baseline[,1]<-c(0.75,0.53,0.364,0.17756911,NA,0.17756911,NA,0.17756911)
-  precent.person.baseline[,2]<-c(0.25,0.47,0.626,0.25345185,NA,0.25345185,NA,0.25345185)
-  precent.person.baseline[,3]<-c(0.0000000667,0.0000000667,0.01,0.56897904,NA,0.56897904,NA,0.56897904)
+  precent.person.baseline[,1]<-person.vehicle.distance_input[10:17,3]
+  precent.person.baseline[,2]<-person.vehicle.distance_input[19:26,3]
+  precent.person.baseline[,3]<-person.vehicle.distance_input[28:35,3]
   
   # matrix of distance (person) by road types and modes for baseline (miles per day)
   distance.baseline.person <- t.person.distance.baseline * precent.person.baseline
    
   # matrix of occupancy
   # source: hard coded estimate & CHTS2012
-  occ.baseline <- c(1,1,10,1.74763709,NA,1,NA,1.00069988)
+  occ.baseline <- person.vehicle.distance_input[38:45,3]
   
-  # for the scenario case
-  if (scenario.no ==1 | scenario.no==2 | scenario.no==3){
-    # person distance (person) for all road types and all modes (miles per day)
-    # data source: MTC Travel Model One
-    t.person.distance.scenario <- c(0.449455623452742,0.0864333319829324,0.282430577165011,18.5556107757615,NA,0.0000007664437775167,NA,0.12788347)
-    
-    # matrix of distance (person) distribution by road types and modes for scenario (%)
-    # data source: for walk and bike, hard coded estimate; for other modes, MTC Travel Model One
-    precent.person.scenario <- matrix(NA,nrow=nTrafficModeV,ncol=3,dimnames=list(ModeNames[1:8],c("local","arterial","highway")))
-    precent.person.scenario[,1] <- c(0.75,0.53,0.075048750103747,0.0810998439977028,NA,0.075048750103747,NA,0.0810998439977028)
-    precent.person.scenario[,2] <- c(0.25,0.47,0.252611107543058,0.288307729519899,NA,0.252611107543058,NA,0.288307729519899)
-    precent.person.scenario[,3] <- c(0.0000000667,0.0000000667,0.672340142353195,0.630592426482398,NA,0.672340142353195,NA,0.630592426482398)
-    
-    # matrix of distance (person) by road types for scenario (miles per day)
-    distance.scenario.person <- t.person.distance.scenario * precent.person.scenario
-
-    # matrix of occupancy
-    # source: hard coded estimate & MTC Travel Model One
-    occ.scenario <- c(1,1,10,1.33434155662758,NA,1,NA,1.00069988)
-        
-  }else if (scenario.no ==4){
-    # person distance (person) for all road types and all modes (miles per day)
-    # data source: MTC Travel Model One
-    t.person.distance.scenario <- c(0.623498784292235,2.49399513716894,2.875830553354,21.4168339676784,0.583379853097641,1.70441975367477,NA,0.127883472083225)
-    
-    # matrix of distance (person) distribution by road types and modes for scenario (%)
-    # data source: baseline distribution
-    precent.person.scenario <- matrix(NA,nrow=nTrafficModeV,ncol=3,dimnames=list(ModeNames[1:8],c("local","arterial","highway")))
-    precent.person.scenario[,1] <- c(0.75,0.53,0.364,0.17756911,NA,0.17756911,NA,0.17756911)
-    precent.person.scenario[,2] <- c(0.25,0.47,0.626,0.25345185,NA,0.25345185,NA,0.25345185)
-    precent.person.scenario[,3] <- c(0.0000000667,0.0000000667,0.01,0.56897904,NA,0.56897904,NA,0.56897904)
-    
-    # matrix of distance (person) by road types for scenario (miles per day)
-    distance.scenario.person <- t.person.distance.scenario * precent.person.scenario
-    
-    # adjust the value for mode "car"
-    for (i in 1:3) {
-      testvalue<-distance.baseline.person[4,i]-sum(distance.scenario.person[1:3,i])+sum(distance.baseline.person[1:3,i])-t.person.distance.scenario[5]*precent.person.scenario[3,i]+t.person.distance.baseline[5]*precent.person.scenario[3,i]
-      ifelse (testvalue<0,distance.scenario.person[4,i]<-0.01,distance.scenario.person[4,i]<-testvalue) 
-    }
-     
-    # source: hard coded estimate & MTC Travel Model One
-    occ.scenario <- c(1,1,10,1.74763708716959,NA,1,NA,1.00069988)
-    
-  }else {  
-    return("Wrong scenario number. Please input a number among 1,2,3,and 4.")
-  }
+  # for scenario
+  
+  #person distance (person) for all road types and all modes (miles per day)
+  # data source: MTC Travel Model One
+  t.person.distance.scenario <- person.vehicle.distance_input[1:8,5]
+  
+  # matrix of distance (person) distribution by road types and modes for scenario (%)
+  # data source: for walk and bike, hard coded estimate; for other modes, MTC Travel Model One
+  precent.person.scenario <- matrix(NA,nrow=nTrafficModeV,ncol=3,dimnames=list(ModeNames[1:8],c("local","arterial","highway")))
+  precent.person.scenario[,1] <- person.vehicle.distance_input[10:17,5]
+  precent.person.scenario[,2] <- person.vehicle.distance_input[19:26,5]
+  precent.person.scenario[,3] <- person.vehicle.distance_input[28:35,5]
+  
+  # matrix of distance (person) by road types for scenario (miles per day)
+  distance.scenario.person <- t.person.distance.scenario * precent.person.scenario
+  
+  # matrix of occupancy
+  # source: hard coded estimate & MTC Travel Model One
+  occ.scenario <- person.vehicle.distance_input[38:45,5]
+  
+  # # for the scenario case
+  # if (scenario.no ==1 | scenario.no==2 | scenario.no==3){
+  #   # person distance (person) for all road types and all modes (miles per day)
+  #   # data source: MTC Travel Model One
+  #   t.person.distance.scenario <- c(0.449455623452742,0.0864333319829324,0.282430577165011,18.5556107757615,NA,0.0000007664437775167,NA,0.12788347)
+  #   
+  #   # matrix of distance (person) distribution by road types and modes for scenario (%)
+  #   # data source: for walk and bike, hard coded estimate; for other modes, MTC Travel Model One
+  #   precent.person.scenario <- matrix(NA,nrow=nTrafficModeV,ncol=3,dimnames=list(ModeNames[1:8],c("local","arterial","highway")))
+  #   precent.person.scenario[,1] <- c(0.75,0.53,0.075048750103747,0.0810998439977028,NA,0.075048750103747,NA,0.0810998439977028)
+  #   precent.person.scenario[,2] <- c(0.25,0.47,0.252611107543058,0.288307729519899,NA,0.252611107543058,NA,0.288307729519899)
+  #   precent.person.scenario[,3] <- c(0.0000000667,0.0000000667,0.672340142353195,0.630592426482398,NA,0.672340142353195,NA,0.630592426482398)
+  #   
+  #   # matrix of distance (person) by road types for scenario (miles per day)
+  #   distance.scenario.person <- t.person.distance.scenario * precent.person.scenario
+  # 
+  #   # matrix of occupancy
+  #   # source: hard coded estimate & MTC Travel Model One
+  #   occ.scenario <- c(1,1,10,1.33434155662758,NA,1,NA,1.00069988)
+  #       
+  # }else if (scenario.no ==4){
+  #   # person distance (person) for all road types and all modes (miles per day)
+  #   # data source: MTC Travel Model One
+  #   t.person.distance.scenario <- c(0.623498784292235,2.49399513716894,2.875830553354,21.4168339676784,0.583379853097641,1.70441975367477,NA,0.127883472083225)
+  #   
+  #   # matrix of distance (person) distribution by road types and modes for scenario (%)
+  #   # data source: baseline distribution
+  #   precent.person.scenario <- matrix(NA,nrow=nTrafficModeV,ncol=3,dimnames=list(ModeNames[1:8],c("local","arterial","highway")))
+  #   precent.person.scenario[,1] <- c(0.75,0.53,0.364,0.17756911,NA,0.17756911,NA,0.17756911)
+  #   precent.person.scenario[,2] <- c(0.25,0.47,0.626,0.25345185,NA,0.25345185,NA,0.25345185)
+  #   precent.person.scenario[,3] <- c(0.0000000667,0.0000000667,0.01,0.56897904,NA,0.56897904,NA,0.56897904)
+  #   
+  #   # matrix of distance (person) by road types for scenario (miles per day)
+  #   distance.scenario.person <- t.person.distance.scenario * precent.person.scenario
+  #   
+  #   # adjust the value for mode "car"
+  #   for (i in 1:3) {
+  #     testvalue<-distance.baseline.person[4,i]-sum(distance.scenario.person[1:3,i])+sum(distance.baseline.person[1:3,i])-t.person.distance.scenario[5]*precent.person.scenario[3,i]+t.person.distance.baseline[5]*precent.person.scenario[3,i]
+  #     ifelse (testvalue<0,distance.scenario.person[4,i]<-0.01,distance.scenario.person[4,i]<-testvalue) 
+  #   }
+  #    
+  #   # source: hard coded estimate & MTC Travel Model One
+  #   occ.scenario <- c(1,1,10,1.74763708716959,NA,1,NA,1.00069988)
+  #   
+  # }else {  
+  #   return("Wrong scenario number. Please input a number among 1,2,3,and 4.")
+  # }
      
   # matrix of distance (vehicle) by road types (miles per day)
   distance.baseline.vehicle <- distance.baseline.person/occ.baseline
@@ -212,8 +235,8 @@ createInjuryResults <- function(injury.baseline,injury.scenario){
 #obtain the ITHIM sheet "Baseline injuries"
 injury.baseline <- createBaselineInjury()
 
-#obtain the ITHIM sheet "Dist by rode type" after inputting the scenario number (1,2,3 or 4, using 4 as an example here)
-dist <-  createDistribution(4) 
+#obtain the ITHIM sheet "Dist by rode type"
+dist <-  createDistribution() 
 
 #compute the "Scenario Injury Multiplier"
 scenario.multiplier <- createScenarioInjuryMultiplier(dist)
