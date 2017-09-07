88ddf4e93bd523b407fe34005cb56cd408929d01
Yizheng Wu <Yizheng@Yizhengs-MBP.local>
2017-09-07 08:39:27 -0700

    bug fixing

--- 00_R scripts/EquityAnalysis_ITHIM_ShinyApp.R
+++ 00_R scripts/EquityAnalysis_ITHIM_ShinyApp.R
@@ -21,15 +21,15 @@ source('EquityAnalysis_ITHIM_Injuries_TwoRaces.R')
 integrated.shiny.app <- function(countyID,barID,outcomeID,demogrID,yaxisID){
   if (outcomeID == 1){
     if (yaxisID == 1){
-      plot.shiny.app.PA(countyID = countyID,dbID = 1,typeID = 1,demogrID = demogrID,barID = 1)
+      plot.shiny.app.PA(countyID = countyID,dbID = 1,typeID = 1,demogrID = demogrID,barID = barID)
     }else if (yaxisID==2){
-      plot.shiny.app.PA(countyID = countyID,dbID = 1,typeID = 2,demogrID = demogrID,barID = 1)
+      plot.shiny.app.PA(countyID = countyID,dbID = 1,typeID = 2,demogrID = demogrID,barID = barID)
     }else if (yaxisID==3){
-      plot.shiny.app.PA(countyID = countyID,dbID = 2,typeID = 1,demogrID = demogrID,barID = 1)
+      plot.shiny.app.PA(countyID = countyID,dbID = 2,typeID = 1,demogrID = demogrID,barID = barID)
     }else if (yaxisID==4){
-      plot.shiny.app.PA(countyID = countyID,dbID = 2,typeID = 2,demogrID = demogrID,barID = 1)
+      plot.shiny.app.PA(countyID = countyID,dbID = 2,typeID = 2,demogrID = demogrID,barID = barID)
     }else if (yaxisID==5){
-      plot.shiny.app.PA(countyID = countyID,dbID = 1,typeID = 3,demogrID = demogrID,barID = 1)
+      plot.shiny.app.PA(countyID = countyID,dbID = 1,typeID = 3,demogrID = demogrID,barID = barID)
     }else{
       message('wrong input')
     }
@@ -175,7 +175,7 @@ integrated.shiny.app <- function(countyID,barID,outcomeID,demogrID,yaxisID){
 # outcomeID: 1-physical activity; 2-injury; 3-both
 # demogrID: 1-Race/ethnicty; 2-household income
 # yaxisID: 1-Death total; 2-Death age.std; 3-DALYs total; 4-DALYs age.std; 5-physical activity data
-integrated.shiny.app(countyID = 7, barID = 4,outcomeID = 2,demogrID = 1, yaxisID = 1)
+integrated.shiny.app(countyID = 7, barID = 3,outcomeID = 1,demogrID = 1, yaxisID = 1)
 
 
 
